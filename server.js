f27c36d9f8bf9cd75ad1a0b04e130db804ccd046
Ayush Harsh <ayushharshashu@gmail.com>
2021-08-27 09:41:54 +0530

    Get current user profile

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get('/', (req,res) => res.send('API running'));
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.POST || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
