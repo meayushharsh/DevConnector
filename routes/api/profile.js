32728379b7c9596c191eafb027675e9eaeb5b9ac
Ayush Harsh <ayushharshashu@gmail.com>
2021-08-27 15:50:13 +0530

    Delete profile and users

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -131,4 +131,19 @@ router.get('/user/:user_id', async (req,res) => {
     }
 });
 
+// @route    DELETE api/profile
+// #desc     Delete user, profile and posts
+// @access   Private
+
+router.delete('/', auth, async (req,res) => {
+    try {
+        await Profile.findOneAndRemove({ user: req.user.id });
+        await User.findOneAndRemove({ _id: req.user.id });
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.log(err.meaasge);
+        res.status(500).send("Server Error");
+    }
+});
+
 module.exports = router;
