8c3fd02f48ce5e2dbe4cf81c4443a3baf67fa182
Ayush Harsh <ayushharshashu@gmail.com>
2021-08-29 15:30:49 +0530

    Get user repos from Github

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const config = require('config');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const auth = require('../../middleware/auth');
@@ -191,7 +193,7 @@ router.put('/experience', [ auth, [
     }
 });
 
-// @route    DELETE api/experience/:exp_id
+// @route    DELETE api/profile/experience/:exp_id
 // #desc     Delete a profile experience
 // @access   Private
 
@@ -257,7 +259,7 @@ router.put('/education', [ auth, [
     }
 });
 
-// @route    DELETE api/education/:edu_id
+// @route    DELETE api/profile/education/:edu_id
 // #desc     Delete a profile education
 // @access   Private
 
@@ -276,5 +278,27 @@ router.delete('/education/:edu_id', auth, async (req,res) => {
         res.status(500).send("Server Error");
     }
 });
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+
+router.get('/github/:username', async (req, res) => {
+    try {
+      const uri = encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      );
+      const headers = {
+        'user-agent': 'node.js',
+        Authorization: `token ${config.get('githubToken')}`
+      };
+  
+      const gitHubResponse = await axios.get(uri, { headers });
+      return res.json(gitHubResponse.data);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(404).json({ msg: 'No Github profile found' });
+    }
+  });
 
 module.exports = router;
