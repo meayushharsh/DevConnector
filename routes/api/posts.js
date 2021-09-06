346f58e3d462f0f231d653537f0e3754b143c01e
Ayush Harsh <ayushharshashu@gmail.com>
2021-09-07 02:15:12 +0530

    Unlike an already liked post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -110,9 +110,26 @@ router.put('/like/:id', auth, async (req,res) => {
         res.json(post.likes);
     } catch (err) {
         console.error(err.message);
-        if(err.kind === 'ObjectId') {
-            return res.status(404).json({ msg: 'Post not found' });
+        res.status(500).json('Server Error');
+    }
+});
+
+// @route    PUT api/posts/unlike/:id
+// #desc     Unlike an already liked post
+// @access   Private
+
+router.put('/unlike/:id', auth, async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: 'Post has not been liked' });
         }
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+        post.likes.splice(removeIndex,1);
+        await post.save();
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
         res.status(500).json('Server Error');
     }
 });
