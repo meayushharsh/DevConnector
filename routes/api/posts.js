51d4c12004b55bfa9af0b55b337a13263054066a
Ayush Harsh <ayushharshashu@gmail.com>
2021-09-07 03:00:42 +0530

    Delete Comment on a Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -166,4 +166,34 @@ router.post('/comment/:id', [ auth, [
     
 } );
 
+// @route    DELETE api/posts/comment/:id/:comment_id
+// #desc     Delete comment on a post
+// @access   Private
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+  
+      const comment = post.comments.find(
+        (comment) => comment.id === req.params.comment_id
+      );
+      if (!comment) {
+        return res.status(404).json({ msg: 'Comment does not exist' });
+      }
+      if (comment.user.toString() !== req.user.id) {
+        return res.status(401).json({ msg: 'User not authorized' });
+      }
+  
+      const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+      post.comments.splice(removeIndex,1);
+  
+      await post.save();
+  
+      return res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server Error');
+    }
+  });
+
 module.exports = router;
