2af13ebb977816d7ff0c248c989862fec4ab1e88
Ayush Harsh <ayushharshashu@gmail.com>
2021-09-07 00:05:16 +0530

    Get All Posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -37,4 +37,18 @@ router.post('/', [ auth, [
     
 } );
 
+// @route    GET api/posts
+// #desc     Get all Posts
+// @access   Private
+
+router.get('/', auth, async (req,res) => {
+    try {
+        const posts = await Post.find().sort({ date:-1 });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json('Server Error');
+    }
+});
+
 module.exports = router;
